a0224f96f85a6d6c1b007a6c185353c60ec9329e
ckinkeldey <christoph.kinkeldey@gmx.de>
2015-06-19 12:33:04 +1000

    Closes #4

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ var backgroundPath = "./data/map.png";
 
 var eps = 10;
 
+var route = [];
 // point at end of selected route
 var currentEnd;
 
@@ -83,10 +84,13 @@ d3.json("data/roads.topojson", function(error, vectordata) {
 		.attr("id", function(d) {return "road" + d.properties.id;})
 		.each(setColor);
 	
-	roads.on("mousedown", function() {
+	roads.on("click", function() {
 		var self = d3.select(this);
 		var id = self.attr("id");
 		var selected = d3.select("#" + id +"_select");
+		if (!isValid(selected)) {
+			return;
+		}
 		var active = (selected.attr("active") == "true") ? false : true;
 		console.log("active = " + selected.attr("active"));
 		console.log("new active = " + active);
@@ -94,6 +98,13 @@ d3.json("data/roads.topojson", function(error, vectordata) {
 		console.log("new opacity = " + newOpacity);
 		selected.style("opacity", newOpacity);
 		selected.attr("active", active);
+		// add selected part to the route
+		route.push(self);
+		updateEndPoint(self);
+		if (isRouteComplete(self)) {
+			selectionlayer.selectAll("path")
+			.style("stroke", "green");
+		}
 	});
 	
 	roads.on("mouseover", function() {
@@ -115,6 +126,24 @@ d3.json("data/roads.topojson", function(error, vectordata) {
 	});	
 });
 
+function updateEndPoint(segment) {
+	var start = segment.node().getPointAtLength(0);
+	var end = segment.node().getPointAtLength(segment.node().getTotalLength());
+	var p0 = new toxi.geom.Vec2D(start.x, start.y);
+	var p1 = new toxi.geom.Vec2D(end.x, end.y);
+	if (p0.distanceTo(currentEnd) < eps) {
+		currentEnd = p1;
+	} else if (p1.distanceTo(currentEnd) < eps) {
+		currentEnd = p0;
+	}
+}
+
+function isRouteComplete(segment) {
+	var pointB = d3.select("#p1").node().getPointAtLength(0);
+	var b = new toxi.geom.Vec2D(pointB.x, pointB.y);
+	return b.distanceTo(currentEnd) < eps;
+}
+
 d3.json("data/AB.topojson", function(error, pointdata) {
 	if (error)
 		throw error;
@@ -131,24 +160,17 @@ d3.json("data/AB.topojson", function(error, pointdata) {
 		.style("stroke", "red")
 		.append("text")
 	;
-	
+	// initial state: starting point is current end of route
 	var pointA = d3.select("#p0").node().getPointAtLength(0);
 	currentEnd = new toxi.geom.Vec2D(pointA.x, pointA.y);
-	console.log("current end: " + currentEnd);
+	// console.log("current end: " + currentEnd);
 });
 
 function isValid(path) {
 	var start = path.node().getPointAtLength(0);
-	var stop = path.node().getPointAtLength(path.node().getTotalLength());
+	var end = path.node().getPointAtLength(path.node().getTotalLength());
 	var p0 = new toxi.geom.Vec2D(start.x, start.y);
-	var p1 = new toxi.geom.Vec2D(stop.x, stop.y);
-		if (p0.distanceTo(currentEnd) < eps) {
-			currentEnd = p1;
-			return true;
-		} else if (p1.distanceTo(currentEnd) < eps) {
-			currentEnd = p0;
-			return true;
-		}
-	return false;
+	var p1 = new toxi.geom.Vec2D(end.x, end.y);
+	return p0.distanceTo(currentEnd) < eps || p1.distanceTo(currentEnd) < eps;
 }
 
