539a3dcfbf3e48a1f2242ff8d3b9adaf1ec7c779
ckinkeldey <christoph@kinkeldey.de>
2015-07-20 17:54:16 +1000

    Closes #38

--- js/sdm-main.js
+++ js/sdm-main.js
@@ -2,7 +2,9 @@
 	                 "explicit-symbol.html", "explicit-symbol-m.html",
 	                 "explicit-texture.html", "explicit-texture-m.html"
 	                 ];
-
+	if (random) {
+		shuffle(scenarios);
+	}
 	var i = 0;
 	$("#deleteButton").addClass("invisible");
 	$("#textInfo").addClass("invisible");
@@ -22,4 +24,23 @@
 			$("#mainpage").load("sdm-end.html"); 
 		}
 	}
+	
+	function shuffle(array) {
+		  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+		  // While there remain elements to shuffle...
+		  while (0 !== currentIndex) {
+
+		    // Pick a remaining element...
+		    randomIndex = Math.floor(Math.random() * currentIndex);
+		    currentIndex -= 1;
+
+		    // And swap it with the current element.
+		    temporaryValue = array[currentIndex];
+		    array[currentIndex] = array[randomIndex];
+		    array[randomIndex] = temporaryValue;
+		  }
+
+		  return array;
+		}
 	
