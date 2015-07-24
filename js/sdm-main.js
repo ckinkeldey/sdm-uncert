4fd0663a772b6e943bd0a212a90d3a78e62d2ab0
ckinkeldey <christoph@kinkeldey.de>
2015-07-24 17:00:57 +1000

    New scenarios (2)

--- js/sdm-main.js
+++ js/sdm-main.js
@@ -1,12 +1,15 @@
 var scenarios = [ "explicit-color-1a.html", "explicit-color-m-1a.html",
                   "explicit-color-1b.html", "explicit-color-m-1b.html",
-                  "explicit-color-2.html", "explicit-color-m-2.html",
+                  "explicit-color-2a.html", "explicit-color-m-2a.html",
+                  "explicit-color-2b.html", "explicit-color-m-2b.html",
                   "explicit-symbol-1c.html", "explicit-symbol-m-1c.html",
                   "explicit-symbol-1d.html", "explicit-symbol-m-1d.html",
-                  "explicit-symbol-2.html", "explicit-symbol-m-2.html",
+                  "explicit-symbol-2c.html", "explicit-symbol-m-2c.html",
+                  "explicit-symbol-2d.html", "explicit-symbol-m-2d.html",
                   "explicit-texture-1e.html", "explicit-texture-m-1e.html", 
                   "explicit-texture-1f.html", "explicit-texture-m-1f.html", 
-                  "explicit-texture-2.html", "explicit-texture-m-2.html"
+                  "explicit-texture-2e.html", "explicit-texture-m-2e.html",
+                  "explicit-texture-2f.html", "explicit-texture-m-2f.html"
                   ];
 if (random) {
 	shuffle(scenarios);
