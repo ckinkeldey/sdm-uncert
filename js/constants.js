aaf78b6557f03bfc4f2616c13b4e57e157ee3ecf
ckinkeldey <christoph@kinkeldey.de>
2015-07-28 18:21:19 +1000

    ....

---
+++ js/constants.js
@@ -0,0 +1,19 @@
+		var A_B_LABEL_POSITION_1A = [ -30, 0, 25, 35 ];
+		var A_B_LABEL_POSITION_1B = [ -20, -20, 15, -25 ];
+		var A_B_LABEL_POSITION_1C = [ -25, 35, 25, -15 ];
+		var A_B_LABEL_POSITION_1D = [ -30, 0, 35, 25 ];
+
+		var A_B_LABEL_POSITION_2A = [ -25, 35, -10, 50 ];
+		var A_B_LABEL_POSITION_2B = [ -20, -20, -25, -10 ];
+		var A_B_LABEL_POSITION_2C = [ 30, 10, 35, -10 ];
+		var A_B_LABEL_POSITION_2D = [ -25, 35, -10, 50 ];
+		
+		var RISKS_1A = [ 0, 0.2, 0.10, 0.05, 0.025 ];
+		var RISKS_1B = [ 0, 0.2, 0.10, 0.05, 0.025 ];
+		var RISKS_1C = [ 0, 0.2, 0.10, 0.05, 0.025 ];
+		var RISKS_1D = [ 0, 0.2, 0.10, 0.08, 0.04 ];		
+		
+		var RISKS_2A = [ 0, 0.3, 0.10, 0.05, 0.025 ];
+		var RISKS_2B = [ 0, 0.2, 0.15, 0.05, 0.025 ];
+		var RISKS_2C = [ 0, 0.2, 0.10, 0.04, 0.025 ];
+		var RISKS_2D = [ 0, 0.18, 0.14, 0.1, 0.08 ];
